b33f8ba8193cd235b6e2cd688cafa9e0fcc557c8
HoQuangDat <hoquangdat001@gmail.com>
2024-10-03 14:50:05 +0700

    update DepositForm

--- src/app/UserContext.js
+++ src/app/UserContext.js
@@ -1,15 +1,3 @@
-import React, { useState } from "react";
+import React from "react";
 
-// Tạo context
-export const UserContext = React.createContext(null);
-
-// Tạo provider để cung cấp giá trị cho các component con
-export const UserProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null); // State quản lý thông tin user
-
-  return (
-    <UserContext.Provider value={{ userData, setUserData }}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+export const UserContext = React.createContext();
